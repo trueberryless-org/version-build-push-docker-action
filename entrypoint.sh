8f9a25e468026718fa9293c52b1414264fe1db3a
trueberryless <99918022+trueberryless@users.noreply.github.com>
2024-02-05 12:42:51 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,10 @@
 # Read inputs from environment variables
 IMAGE_NAME=$1
 REGISTRY=$2
-DOCKER_USERNAME=$3
-DOCKER_PASSWORD=$4
+REPOSITORY=$3
+TRIGGER_MESSAGE=$4
+DOCKER_USERNAME=$5
+DOCKER_PASSWORD=$6
 
 # Get the latest semantic version numbers (major, minor, patch) from the docker registry image
 CURRENT_VERSION=$(docker pull $REGISTRY/$IMAGE_NAME | grep -oP "(?<=${REGISTRY}/${IMAGE_NAME}:)\d+\.\d+\.\d+")
@@ -24,8 +26,9 @@ echo "version-minor=$MINOR_VERSION" >> "$GITHUB_ENV"
 echo "version-patch=$PATCH_VERSION" >> "$GITHUB_ENV"
 
 # Checkout the original repo
-git clone https://github.com/your-username/your-repo.git
-cd your-repo
+git clone https://github.com/$REPOSITORY.git
+REPO_NAME=$(echo $REPOSITORY | cut -d '/' -f 2)
+cd $REPO_NAME
 
 # Build a docker image and push it to the registry with the new patch version
 docker build -t $REGISTRY/$IMAGE_NAME:v$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION .
